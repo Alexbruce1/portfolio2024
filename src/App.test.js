2c3c3f9d79e352f45d3e07a8806ffc75709d48a3
Alex Bruce <brucealmightyalex@yahoo.com>
2024-09-04 16:47:22 -0600

    Update test to suppress warnings about depricated dependencies, and fix main test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,26 @@
 import { render, screen } from '@testing-library/react';
+import { act } from 'react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+beforeAll(() => {
+  jest.spyOn(console, 'error').mockImplementation((message) => {
+    if (!message.includes('Warning: `ReactDOMTestUtils.act` is deprecated')) {
+      console.error(message);
+    }
+  });
+});
+
+afterAll(() => {
+  console.error.mockRestore();
+});
+
+
+test('renders Alex link', () => {
+  act(() => {
+    render(<App />);
+  });
+
+  const linkElements = screen.getAllByText(/Alex/i);
+  
+  expect(linkElements.length).toBeGreaterThan(0);
 });

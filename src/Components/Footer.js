b8ea58136ff3d2add5ff6c5787c9beac92623f70
Alex Bruce <brucealmightyalex@yahoo.com>
2024-09-04 16:56:32 -0600

    Remove unused component imports and useState

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Footer.css';
 import LI from '../assets/tech-svgs/li.svg';
 import GH from '../assets/tech-svgs/gh.svg';

a6a0257db37c900bb56988573713e1b7b6606a8e
Alex Bruce <brucealmightyalex@yahoo.com>
2024-09-05 10:53:54 -0600

    Add a couple of possible solutions for a bug in the deployed app

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -4,13 +4,33 @@ import ABResume from '../assets/Alex_Bruce_Resume.pdf'
 
 
 function Resume() {
+  const pdfUrl = 'https://abresume.s3.amazonaws.com/Alex_Bruce_Resume.pdf';
+
   return(
     <div className='Resume'>
       <iframe
         className='resume-iframe'
-        src={ABResume}
+        src={pdfUrl}
         title="Resume"
       />
+
+      <object
+        data={pdfUrl}
+        type='application/pdf'
+        className='resume-iframe'
+      >
+        <p>
+          Your browser does not support PDFs. <a href={pdfUrl}>Download the PDF</a>.
+        </p>
+      </object>
+
+
+      
+      <div style={{ marginTop: '20px', textAlign: 'center' }}>
+        <a href={pdfUrl} download="Alex_Bruce_Resume.pdf">
+          Download Resume
+        </a>
+      </div>
     </div>
   )
 }

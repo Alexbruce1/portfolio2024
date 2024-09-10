0bfd01836e28ad33e3b8d02c1ae89796dfabf2b9
Alex Bruce <brucealmightyalex@yahoo.com>
2024-09-10 12:18:51 -0600

    Add styling for contact page

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,5 +1,9 @@
 import React, { useState } from "react";
 import './Contact.css';
+import LI from '../assets/tech-svgs/li.svg';
+import GH from '../assets/tech-svgs/gh.svg';
+import phone from '../assets/tech-svgs/phone.svg';
+
 
 function Contact() {
   const [formData, setFormData] = useState({
@@ -51,45 +55,73 @@ function Contact() {
             <div className="contact-section contact-name-section">
               <label className="contact-label">
                 What's your name?
-                <input
-                  className="contact-field contact-name-input"
-                  type="name"
-                  name="name"
-                  value={formData.name}
-                  onChange={handleChange}
-                  required
-                />
               </label>
+              <input
+                className="contact-field contact-name-input"
+                type="name"
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+                required
+              />
             </div>
             <div className="contact-section contact-email-section">
               <label className="contact-label">
                 What's your email address?
-                <input
-                  className="contact-field contact-email-input"
-                  type="email"
-                  name="email"
-                  value={formData.email}
-                  onChange={handleChange}
-                  required
-                />
               </label>
+              <input
+                className="contact-field contact-email-input"
+                type="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                required
+              />
             </div>
             <div className="contact-section contact-message-section">
               <label className="contact-label">
-                What's your message?
-                <textarea
-                  className="contact-field contact-message-input"
-                  name="message"
-                  value={formData.message}
-                  onChange={handleChange}
-                  required
-                ></textarea>
+                What would you like to say?
               </label>
+              <textarea
+                className="contact-field contact-message-input"
+                name="message"
+                value={formData.message}
+                onChange={handleChange}
+                required
+              ></textarea>
             </div>
             <button className="form-submit" type="submit">Send</button>
             <div className="form-status">Your message was sent</div>
             {/* <div className="form-status">{status}</div> */}
           </form>
+        </div>
+      </div>
+      <div className="contact-links-container">
+        <div className="contact-header">You’re welcome to explore my links below!</div>
+        <div className="contact-form-border">
+          <div className="contact-form">
+            
+
+
+          <a className='contact-link' target="_blank" rel="noreferrer" href='https://www.linkedin.com/in/alexbbruce/'>
+              <img alt='Linkedin icon' src={LI} className='li-icon contact-icon' />
+              <p className='contact-text'>LinkedIn</p>
+            </a>
+            <a className='contact-link' target="_blank" rel="noreferrer" href='https://github.com/Alexbruce1/'>
+              <img alt='Github icon' src={GH} className='li-icon contact-icon' />
+              <p className='contact-text'>GitHub</p>
+            </a>
+            <a className='contact-link contact-link-phone' href="tel:3035643255">
+              <img alt='phone icon' src={phone} className='li-icon contact-icon bw-icon' />
+              <p className='contact-text'>+1(303) 564-3255</p>
+            </a>
+
+
+
+
+
+
+          </div>
         </div>
       </div>
     </div>

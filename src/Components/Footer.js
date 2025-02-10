e1c09696e364c5bc94a179381c937b3f96c92b6d
Alex Bruce <brucealmightyalex@yahoo.com>
2025-02-10 12:18:17 -0700

    Remove unused useEffect and useState imports

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import './Footer.css';
 import LI from '../assets/tech-svgs/li.svg';
